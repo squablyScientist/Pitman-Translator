f87d48292d373a68dbf171ca680c7e6c84261df5
Collin Tod <squablesquable@gmail.com>
2017-08-14 13:15:48 -0400

    Changed mapping method to just using CMU dictionary, more lightweight. Added runtime loading resources from said dictionary

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,8 @@
-import com.sun.speech.freetts.en.us.CMULexicon;
-import com.sun.speech.freetts.lexicon.Lexicon;
-
 import java.io.IOException;
 
 public class Runner {
     public static void main(String[] args) throws IOException {
-        Lexicon lex = CMULexicon.getInstance(true);
-        String[] phones = lex.getPhones("bat", "n");
-        for(String s : phones){
-            System.out.println(s);
-        }
+        TextProc.load();
+        System.out.println(TextProc.lexicon.get("HELLO(1)"));
     }
 }
--- src/TextProc.java
+++ src/TextProc.java
@@ -1,9 +1,14 @@
-import com.sun.speech.freetts.en.us.CMULexicon;
-import com.sun.speech.freetts.lexicon.Lexicon;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
+
 public class TextProc {
 
+    //Path to CMULexicon dictionary
+    private static final String LEXICONPATH = "C:\\Users\\squab\\IdeaProjects\\Pitman-Translator\\src\\strokeFiles\\cmudict-0.7b.txt";
+    static Map<String, List<String>> lexicon;
+    static Map<String, Character> pitmanLex;
 
     /**
      * Splits a bunch of words into an array of a bunch of words
@@ -16,17 +21,19 @@ public class TextProc {
     }
 
     /**
-     * Grabs the phones for a word
-     *
-     * @param s A word
-     * @return An array of phones that correspond to the word given in String s
-     * @throws IOException
+     * Grabs the phones of a word from lexicon hashmap, which is populated by the words withing the CMULexicon
+     * @param s A word that should be in the CMULexicon
+     * @return an array of strings that hold the phones for the word in String s
+     * @throws IllegalArgumentException If the word given is not in the CXMULexicon, then an illegeal argument exception is thrown
      */
-    public static String[] getWordPhones(String s) throws IOException {
-        Lexicon lex = CMULexicon.getInstance(true);
-        return lex.getPhones(s.toLowerCase(), "n");
-    }
+    static String[] getWordPhones(String s) throws IllegalArgumentException{
+        List<String> phoneList;
+        if((phoneList = lexicon.get(s.toUpperCase())) != null){
+            return phoneList.toArray(new String[phoneList.size()]);
+        }
+        throw new IllegalArgumentException("Not in lexicon");
 
+    }
     /**
      * Entire string to phone arrays
      *
@@ -40,11 +47,59 @@ public class TextProc {
         for (int i = 0; i < phones.length; i++) {
             try {
                 phones[i] = getWordPhones(words[i]);
-            } catch (IOException e) {
+            } catch (IllegalArgumentException e) {
                 e.printStackTrace();
 
             }
         }
         return phones;
+    }
+
+    /**
+     * Runtime loading of lexicon mappings
+     */
+    public static void load(){
+        lexicon = new HashMap<>(133910);
+        BufferedReader br;
+        FileReader fr;
+
+        try{
+            fr = new FileReader(LEXICONPATH);
+            br = new BufferedReader(fr);
+
+            //get past cmu copyright stuff
+            for(int i = 0; i < 56; i++){
+                br.readLine();
+            }
+
+            String line;
+            while((line = br.readLine()) != null){
+                mapLex(line);
+            }
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Maps a word to its correct phones based on one line of the CMU lexicon
+     * @param s String of a word and its phones to be mapped to eachother
+     */
+    private static void mapLex(String s){
+
+        Scanner sc = new Scanner(s);
+        List<String> phones = new ArrayList<>();
+        String word = sc.next();
+        while(sc.hasNext()){
+            phones.add(sc.next());
+        }
+        lexicon.put(word, phones);
+        System.out.println("Mapped: " + word + " as " + lexicon.get(word));
+    }
+
+    private static void mapPitLex(String[] s){
+
     }
 }
