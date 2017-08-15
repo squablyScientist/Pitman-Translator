d3ec792ff583942fff39ee67008d335a3691b457
Collin Tod <squablesquable@gmail.com>
2017-08-15 13:42:45 -0400

    Added runtime loading of phoneme to phonetic symbol mapping(many to one, would be messy when going directly to strokes) to be used to match to strokes one to one. Also fixed slight out of bounds bug in finding the length of a word rather than the amount of symbols that it had

--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,13 @@ import java.io.IOException;
 public class Runner {
     public static void main(String[] args) throws IOException {
         TextProc.load();
-        System.out.println(TextProc.lexicon.get("HELLO(1)"));
+        Character[][] symbols = TextProc.phones("hello world");
+        for(int i = 0; i< symbols.length; i++){
+            for(int j = 0; j < symbols[i].length; j++){
+                System.out.print(symbols[i][j]);
+            }
+            System.out.print(" ");
+
+        }
     }
 }
--- src/TextProc.java
+++ src/TextProc.java
@@ -6,9 +6,10 @@ import java.util.*;
 public class TextProc {
 
     //Path to CMULexicon dictionary
-    private static final String LEXICONPATH = "C:\\Users\\squab\\IdeaProjects\\Pitman-Translator\\src\\strokeFiles\\cmudict-0.7b.txt";
-    static Map<String, List<String>> lexicon;
-    static Map<String, Character> pitmanLex;
+    private static final String LEXICONPATH = "src/strokeFiles/cmudict-0.7b.txt";
+    private static final String PITMANLEXPATH = "src/strokeFiles/PitmanLex";
+    private static Map<String, List<Character>> lexicon;
+    private static Map<String, Character> pitmanLex;
 
     /**
      * Splits a bunch of words into an array of a bunch of words
@@ -16,7 +17,7 @@ public class TextProc {
      * @param s The string of multiple words which will be split into an array of each word
      * @return An array of strings, each one word of the string s
      */
-    public static String[] wordsSplit(String s) {
+    private static String[] wordsSplit(String s) {
         return s.split("\\s");
     }
 
@@ -26,10 +27,10 @@ public class TextProc {
      * @return an array of strings that hold the phones for the word in String s
      * @throws IllegalArgumentException If the word given is not in the CXMULexicon, then an illegeal argument exception is thrown
      */
-    static String[] getWordPhones(String s) throws IllegalArgumentException{
-        List<String> phoneList;
+    private static Character[] getWordSymbols(String s) throws IllegalArgumentException{
+        List<Character> phoneList;
         if((phoneList = lexicon.get(s.toUpperCase())) != null){
-            return phoneList.toArray(new String[phoneList.size()]);
+            return phoneList.toArray(new Character[phoneList.size()]);
         }
         throw new IllegalArgumentException("Not in lexicon");
 
@@ -40,13 +41,15 @@ public class TextProc {
      * @param s Any string
      * @return An array of arrays that hold the phones for all words within String s
      */
-    public static String[][] phones(String s) {
+    public static Character[][] phones(String s) {
         String[] words = wordsSplit(s);
-        String[][] phones = new String[s.length()][];
+
+        //2d array of characters with one dimension defined as the amount of words in String s
+        Character[][] phones = new Character[words.length][];
 
         for (int i = 0; i < phones.length; i++) {
             try {
-                phones[i] = getWordPhones(words[i]);
+                phones[i] = getWordSymbols(words[i]);
             } catch (IllegalArgumentException e) {
                 e.printStackTrace();
 
@@ -56,14 +59,25 @@ public class TextProc {
     }
 
     /**
-     * Runtime loading of lexicon mappings
+     * Runtime loading of lexicon mappings of phonemes, symbols, and (NOT IMPLEMENTED) pitman strokes
      */
-    public static void load(){
+     static void load(){
         lexicon = new HashMap<>(133910);
+        pitmanLex = new HashMap<>(84);
         BufferedReader br;
         FileReader fr;
+        String line;
 
         try{
+
+            //Map all phonetic symbols to the phonemes in the CMUDictionary
+            fr = new FileReader(PITMANLEXPATH);
+            br = new BufferedReader(fr);
+            while((line = br.readLine()) != null){
+                mapPitLex(line);
+            }
+
+            //Map all the words in the CMUDictionary to their phonetic symbols
             fr = new FileReader(LEXICONPATH);
             br = new BufferedReader(fr);
 
@@ -72,12 +86,10 @@ public class TextProc {
                 br.readLine();
             }
 
-            String line;
             while((line = br.readLine()) != null){
                 mapLex(line);
             }
         }
-
         catch(IOException e){
             e.printStackTrace();
         }
@@ -88,18 +100,24 @@ public class TextProc {
      * @param s String of a word and its phones to be mapped to eachother
      */
     private static void mapLex(String s){
-
         Scanner sc = new Scanner(s);
-        List<String> phones = new ArrayList<>();
+        List<Character> phones = new ArrayList<>();
         String word = sc.next();
         while(sc.hasNext()){
-            phones.add(sc.next());
+            phones.add(pitmanLex.get(sc.next()));
         }
         lexicon.put(word, phones);
         System.out.println("Mapped: " + word + " as " + lexicon.get(word));
     }
 
-    private static void mapPitLex(String[] s){
-
+    /**
+     * Maps a phoneme to it's phonetic character based on a many to one mapping file
+     * @param s String made up of a phoneme and a phonetic character separated by a space
+     */
+    private static void mapPitLex(String s){
+        Scanner sc = new Scanner(s);
+        String phone = sc.next();
+        pitmanLex.put(phone, sc.next().charAt(0));
+        System.out.println("Mapped " + phone + " as " + pitmanLex.get(phone));
     }
 }
