238fea864725fe175dd32cce48c56f0bce51d265
Collin Tod <squablesquable@gmail.com>
2017-11-02 11:42:01 -0400

    Implemented Stroke methods

--- src/Stroke.java
+++ src/Stroke.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+import javafx.scene.image.Image;
 
+import java.awt.*;
+
+/**
+ * Stroke.java
+ *
+ * A class to hold a stroke file, as well as all of its relevant information.
+ *
+ * @author Collin Tod
+ */
 public class Stroke {
+    private Image image;
+    private Point start;
+    private Point end;
+
+    public Stroke(String url, Point start, Point end){
+        this.image = new Image(url);
+        this.start = start;
+        this.end = end;
+    }
+
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Point getStart() {
+        return start;
+    }
+
+    public void setStart(Point start) {
+        this.start.setLocation(start);
+    }
+
+    public void setStart(int x, int y){
+        start.setLocation(x,y);
+    }
+
+    public Point getEnd() {
+        return end;
+    }
+
+    public void setEnd(Point end) {
+        this.end.setLocation(end);
+    }
+
+    public void setEnd(int x, int y){
+       this.end.setLocation(x, y);
+    }
 }
