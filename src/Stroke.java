d90a8a9a80d4e6e04aa288efc49114170229d3af
Collin Tod <squablesquable@gmail.com>
2017-11-02 11:46:17 -0400

    Documented the Stroke class

--- src/Stroke.java
+++ src/Stroke.java
@@ -21,34 +21,75 @@ public class Stroke {
     }
 
 
+    /**
+     * Returns the visual stroke
+     *
+     * @return The image that is to be displayed when representing the stroke
+     */
     public Image getImage() {
         return image;
     }
 
+    /**
+     * Sets the image
+     *
+     * @param image The image that the stroke's image is to be set to
+     */
     public void setImage(Image image) {
         this.image = image;
     }
 
+    /**
+     * Gets the starting point of the stroke within its 80 x 80 png
+     *
+     * @return The starting point
+     */
     public Point getStart() {
         return start;
     }
 
+    /**
+     * Sets the starting point of the stroke
+     *
+     * @param start the point to be set to
+     */
     public void setStart(Point start) {
         this.start.setLocation(start);
     }
 
+    /**
+     * Sets the ending point of the stroke based on coordinates, not another {@code Point}
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public void setStart(int x, int y){
         start.setLocation(x,y);
     }
-
+    /**
+     * Gets the ending point of the stroke within its 80 x 80 png
+     *
+     * @return The ending point
+     */
     public Point getEnd() {
         return end;
     }
 
+    /**
+     * Sets the ending point of the stroke
+     *
+     * @param end the point to be set to
+     */
     public void setEnd(Point end) {
         this.end.setLocation(end);
     }
 
+    /**
+     * Sets the ending point of the stroke based on coordinates, not another {@code Point}
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public void setEnd(int x, int y){
        this.end.setLocation(x, y);
     }
